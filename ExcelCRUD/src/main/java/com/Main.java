69d8493affbb9a605e324e59160d14f28207d718
nVitalii <vnedbaliuk@gmail.com>
2016-09-25 10:04:57 +0200

    add main method for running

---
+++ ExcelCRUD/src/main/java/com/Main.java
@@ -0,0 +1,11 @@
+package com;
+
+import com.crud.CRUDexcel;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CRUDexcel cruDexcel = new CRUDexcel();
+        cruDexcel.createExcel();
+    }
+}
