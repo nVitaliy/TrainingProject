295db1961790eb412fb5b2d25b354de83f6cc342
nVitaliy <vnedbaliuk@gmail.com>
2017-03-02 21:08:29 +0100

    add controllers and html page

---
+++ demoWebApp/src/main/java/com/controllers/ReadingBookController.java
@@ -0,0 +1,39 @@
+package com.controllers;
+
+import com.bookreader.models.Book;
+import com.bookreader.repositories.ReadingBookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/book")
+public class ReadingBookController {
+
+    private ReadingBookRepository readingBook;
+
+    @Autowired
+    public ReadingBookController(ReadingBookRepository readingBook) {
+        this.readingBook = readingBook;
+    }
+
+    @RequestMapping(value = "/book/{reader}", method = RequestMethod.GET)
+    public String readersBooks(@PathVariable("reader") String reader, Model model) {
+
+        List<Book> booksByReaders = readingBook.findByReader(reader);
+        if (booksByReaders != null) {
+            model.addAttribute("books", booksByReaders);
+        }
+        return "book/readingBook";
+    }
+    @RequestMapping(value = "book/{reader}", method = RequestMethod.POST)
+    public String addToReading(@PathVariable("reader") String reader){
+
+        return "redirect:/book/{reader}";
+    }
+}
--- demoWebApp/src/main/java/com/controllers/SampleController.java
+++ demoWebApp/src/main/java/com/controllers/SampleController.java
@@ -1,16 +1,14 @@
 package com.controllers;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@EnableAutoConfiguration
 public class SampleController {
-    @RequestMapping("/")
-    @ResponseBody
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     String home() {
-        return "Hello World!";
+        return "index";
     }
 }
