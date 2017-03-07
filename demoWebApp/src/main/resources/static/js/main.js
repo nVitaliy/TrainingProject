88a6b376535d6933387459a060e8270117b707a5
nVitalii <vnedbaliuk@gmail.com>
2017-03-07 21:56:43 +0100

    add script

--- demoWebApp/src/main/resources/static/js/main.js
+++ demoWebApp/src/main/resources/static/js/main.js
@@ -1,1 +1,35 @@
+/**
+ Upload data to central block
+ */
 
+function loadCenterBlock(fileName) {
+    $('#central_block').load('./view/' + fileName, function (response, status, xhr) {
+        if (status == "error") {
+            var msg = "File uploading error:<br />";
+            $("#central_block").html(msg + xhr.status + " " + xhr.statusText);
+        }
+    });
+}
+
+/**
+ Action after uploading
+ */
+
+$(document).ready(function () {
+    var fileName = location.href.replace(/.*?!/, '');
+    if (fileName != location.href) {
+        loadCenterBlock(fileName)
+    }
+
+});
+
+/**
+ uploading data from html file
+ */
+
+function loadHtmlFile(obj) {
+    var fileName = $(obj).attr('href').replace(/.*?!/, '');
+    if (fileName) {
+        loadCenterBlock(fileName);
+    }
+}
